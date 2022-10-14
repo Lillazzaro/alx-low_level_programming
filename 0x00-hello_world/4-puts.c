4dc0669f2965627794d83e0a4791331b5ec7b1d0
Lillazzaro <omoroje1999@gmail.com>
2022-10-14 14:08:56 -0700

    Include a string of characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 
 int main() {
 
-  puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
+getchar();
 
-  getchar();
-
-    return 0;
+	return (0);
 
    }

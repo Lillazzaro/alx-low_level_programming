15d7dc85b5765148eac1c521cc2d03a6ba2368a6
Lillazzaro <omoroje1999@gmail.com>
2022-10-14 13:58:56 -0700

    Including a string of characters

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//program to output a string of characters
+int main() {
+
+  puts("Programming is like building a multilingual puzzle");
+
+  getchar();
+
+    return 0;
+
+   }

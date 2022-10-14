90a291ddff5b1cfeb1490c92e6ffbe7a05f61e77
Lillazzaro <omoroje1999@gmail.com>
2022-10-14 14:17:19 -0700

    Adding a string of characters

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Serves as entry point tp the program
+ * return - returns zero if no errors else returns non-zero
+ */
 
 int main(void)
 {
 
 puts("\"Programming is like building a multilingual puzzle");
-getchar();
 
 return (0);
 

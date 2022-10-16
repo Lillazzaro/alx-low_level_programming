add5201a8531dc97414e4de18ab13356f92c72d3
Lillazzaro <omoroje1999@gmail.com>
2022-10-16 16:32:57 -0700

    Two set of alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lowercase
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+
+	char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+
+putchar(ch);
+
+}
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+
+putchar('\n');
+
+return (0);
+
+}

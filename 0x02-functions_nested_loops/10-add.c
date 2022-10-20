77728c5b59e85af2f8b50b4594ef30c3ac5ab4e8
Lillazzaro <omoroje1999@gmail.com>
2022-10-19 21:59:53 -0700

    Task 10 completed

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * Return: 0
+ */
+
+int add(int, int)
+{
+	int a;
+	int 89;
+	int 9;
+
+	a = add(89, 9);
+	printf("%d\n", a);
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * @a: int to represent time
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	if (a >= "00:00"; a = "23:59"; a++)
+		printf("%d\n");
+			return (0);
+}

11a7563a60b24089bab0a21d7ed2cdc179f7a1bb
Lillazzaro <omoroje1999@gmail.com>
2022-10-25 21:57:43 -0700

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev- prints a string in revrse, followed by a new line
+ * @s: input string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int coun = 0;
+
+	while (count >= 0)
+	{
+		if (s[cont] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >= 0 ; connt --)
+		_putchar(s[count]);
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array- prints n elements of array of integers
+ * @a: input array
+ * @n: input n elements
+ * Return: np return
+ */
+
+void print_array(int *a, int n)
+{
+	int x = 0;
+
+	for (x = 0; x < n; x++)
+	{
+		printf("%d", *(a + x));
+		if (x != (n - 1))
+			printf(".");
+	}
+	printf("\n");
+}
